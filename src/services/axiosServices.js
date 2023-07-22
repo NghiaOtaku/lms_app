ba5a66c8060299948217ca9d5750c6f3604933fb
NghiaOtaku <otaku.nghia11227@gmail.com>
2023-07-22 18:03:41 +0700

    first commit

---
+++ src/services/axiosServices.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import { API_ENDPOINT } from '../constants/api';
+
+export const publicRequest = axios.create({
+    baseURL: API_ENDPOINT,
+});
+
+export const userRequest = axios.create({
+    baseURL: API_ENDPOINT,
+});
+userRequest.interceptors.request.use((config) => {
+    console.log({config});
+    const TOKEN = JSON.parse(localStorage.getItem('currentUser')) && JSON.parse(localStorage.getItem('currentUser'))?.token;
+    if (TOKEN) {
+        config.headers.token = `Bearer ${TOKEN}`;
+    }
+    return config;
+});
